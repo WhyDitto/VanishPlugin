d69eb47fc755f20416a73385dc88ca320485c3e6
WhyDitto <theditto30@gmail.com>
2022-07-08 14:11:06 -0400

    Possibly fixed damage event as I forgot to add @EventHandler

--- src/main/java/ditto/vanish/Events/damageEvent.java
+++ src/main/java/ditto/vanish/Events/damageEvent.java
@@ -2,11 +2,12 @@ package ditto.vanish.Events;
 
 import ditto.vanish.Main;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class damageEvent implements Listener {
-
+    @EventHandler
     public void onDamage(EntityDamageByEntityEvent event){
         if(event.getEntity() instanceof Player){
             Player player = (Player) event.getEntity();
